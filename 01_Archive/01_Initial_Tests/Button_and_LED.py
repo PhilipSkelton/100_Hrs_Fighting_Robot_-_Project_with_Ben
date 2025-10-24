3f40ddedc45c959a320bc5e8d843c7ca6381cb01
PhilipSkelton <philipskelton14@gmail.com>
2025-10-24 12:35:50 +0100

    Initial Commit

---
+++ 01_Archive/01_Initial_Tests/Button_and_LED.py
@@ -0,0 +1,14 @@
+import gpiozero
+import time
+
+
+button = gpiozero.Button(17)
+led = gpiozero.LED(4)
+
+while True:
+        if button.is_pressed:
+                print("1")
+                led.on()
+        else:
+                print("0")
+                led.off()
